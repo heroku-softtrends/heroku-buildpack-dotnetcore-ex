3236f6d81dd80436beab356b3fb0ca653d3b4f31
Heroku Softtrends <heroku@softtrends.com>
2023-10-12 14:18:25 +0530

    Update common.sh
    start project setup

--- bin/util/common.sh
+++ bin/util/common.sh
@@ -137,9 +137,9 @@ get_project_name() {
 # input - $1
 get_start_project_name() {
 	local project_name=""
-	local launchSettingsFile=$(x=$(dirname $(find $1 -maxdepth 3 -type f | head -1)); while [[ "$x" =~ $1 ]] ; do find "$x" -maxdepth 1 -name launchSettings.json; x=`dirname "$x"`; done)
+	local launchSettingsFile=$(x=$(dirname $(find $1 -maxdepth 1 -type f | head -1)); while [[ "$x" =~ $1 ]] ; do find "$x" -maxdepth 3 -name launchSettings.json; x=`dirname "$x"`; done)
 	if [[ $launchSettingsFile ]]; then
-		project_name=$(basename $(dirname $(dirname ${launchSettingsFile%.*})))
+		project_name=$(dirname ${launchSettingsFile%.*})
 	fi
 	echo $project_name
 }
